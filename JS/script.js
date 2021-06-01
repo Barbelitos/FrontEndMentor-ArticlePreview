6cf639765d27923c82e33a8c3a7d97733624e720
David Fonseca <davidfonseca@davids-mbp.lan>
2021-06-01 10:43:07 +0300

    Add share menu open for desktop screens

--- JS/script.js
+++ JS/script.js
@@ -1,15 +1,8 @@
 const shareButton = document.querySelector(".author__share");
-const authorPicture = document.querySelector(".author__picture");
-const authorInfo = document.querySelector(".author__info");
-const authorArea = document.querySelector(".card__author");
 const shareMenu = document.querySelector(".share__menu");
 const shareButtonArrow = document.querySelector(".share__path");
 
 shareButton.addEventListener("click", () => {
-  authorPicture.classList.toggle("remove");
-  authorInfo.classList.toggle("remove");
-
-  authorArea.classList.toggle("bg__change");
   shareMenu.classList.toggle("share__menu");
   shareMenu.classList.toggle("show__menu");
 
